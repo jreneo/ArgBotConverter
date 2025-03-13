6441215da8bbf92d321efef3541a562cdbbd0ed0
jreneo <jayreneo123@gmail.com>
2025-03-12 20:12:37 -0400

    Added pytubefix and implemented video downloading

--- src/downloader.py
+++ src/downloader.py
@@ -1,11 +1,20 @@
-from pytube import YouTube
-
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
 def download(url : str) -> bool:
     print("Download function:")
+    try:
+        yt = YouTube(
+            url,
+            on_progress_callback=on_progress
+        )
+        print(yt.title)
+        stream = yt.streams.get_highest_resolution()
+        stream.download()
+        print("Downloading...")
+        
+        print("Download complete!")
 
-    
-    if str:
-        return True
-    else:
-        return False
+    except Exception as e:
+        print(f"An error occured: {e}")
+        
